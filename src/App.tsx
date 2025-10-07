6c317771a4182843d2bd20fedfd573a81a828bab
Anaïs Boo <anaisb@hokla.com>
2025-10-07 14:14:27 +0200

    fix: correct typo in prompt message

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
+      <p>Start prompting (or editing) to see magic happen )</p>
     </div>
   );
 }
